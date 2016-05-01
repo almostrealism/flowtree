f44b840957a841d3c7d403f35ca64e0daa2559df
ashesfall <ashesfall@almostrealism.com>
2016-05-01 14:08:41 -0700

    Removed unused imports.

--- PhotonField/src/net/sf/j3d/physics/pfield/util/PriorityQueue.java
+++ PhotonField/src/net/sf/j3d/physics/pfield/util/PriorityQueue.java
@@ -1,9 +1,5 @@
 package net.sf.j3d.physics.pfield.util;
 
-import java.util.ArrayList;
-import java.util.Iterator;
-import java.util.List;
-import java.util.Set;
 import java.util.SortedSet;
 import java.util.TreeSet;
 
