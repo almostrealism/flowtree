16f87b32ecc68663f7f239114b512168f474fd27
ashesfall <ashesfall@almostrealism.com>
2016-05-01 18:18:43 -0700

    Updated documentation and copyright.

--- Rings/src/com/almostrealism/raytracer/engine/SurfaceWrapper.java
+++ Rings/src/com/almostrealism/raytracer/engine/SurfaceWrapper.java
@@ -1,21 +1,12 @@
 /*
- * Copyright (C) 2004  Mike Murray
- *
- *  This program is free software; you can redistribute it and/or modify
- *  it under the terms of the GNU General Public License (version 2)
- *  as published by the Free Software Foundation.
- *
- *  This program is distributed in the hope that it will be useful,
- *  but WITHOUT ANY WARRANTY; without even the implied warranty of
- *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- *  GNU General Public License for more details.
- *
+ * Copyright (C) 2004-16  Mike Murray
+ * All rights reserved.
  */
 
 package com.almostrealism.raytracer.engine;
 
 /**
- * A SurfaceWrapper implementation stores/wraps a Surface.
+ * A {@link SurfaceWrapper} implementation stores/wraps a Surface.
  * 
  * @author Mike Murray
  */
