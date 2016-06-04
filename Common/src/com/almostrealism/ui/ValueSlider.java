c6da72c5d55ab4f9ff54999e511dd2e0dacef736
ashesfall <ashesfall@almostrealism.com>
2016-06-04 16:06:57 -0700

    A simple user interface widget for selecting a floating point value with a slider.

--- Common/src/com/almostrealism/ui/ValueSlider.java
+++ Common/src/com/almostrealism/ui/ValueSlider.java
@@ -1,5 +1,22 @@
 package com.almostrealism.ui;
 
-public class ValueSlider {
+import javax.swing.JSlider;
 
+import com.almostrealism.util.ValueProducer;
+
+public class ValueSlider extends JSlider implements ValueProducer {
+	private static final int scale = 10000;
+	
+	private double min, max;
+	
+	public ValueSlider(int orient, double min, double max, double value) {
+		super(orient, 0, scale);
+		this.min = min;
+		this.max = max;
+		this.setValue((int) (scale * (value - min) / (max - min)));
+	}
+	
+	public double value() {
+		return (max - min) * (getValue() / (double) scale) + min;
+	}
 }
