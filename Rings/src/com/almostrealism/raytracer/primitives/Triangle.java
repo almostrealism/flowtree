8d2bb2f57d5122671ede38774b79f4d4bc9463c3
ashesfall <ashesfall@almostrealism.com>
2016-06-05 00:06:17 -0700

    Minor changes.

--- Rings/src/com/almostrealism/raytracer/primitives/Triangle.java
+++ Rings/src/com/almostrealism/raytracer/primitives/Triangle.java
@@ -1,15 +1,5 @@
 /*
- * Copyright (C) 2004  Mike Murray
- *
- *  This program is free software; you can redistribute it and/or modify
- *  it under the terms of the GNU General Public License (version 2)
- *  as published by the Free Software Foundation.
- *
- *  This program is distributed in the hope that it will be useful,
- *  but WITHOUT ANY WARRANTY; without even the implied warranty of
- *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- *  GNU General Public License for more details.
- *
+ * Copyright (C) 2004-2016  Mike Murray
  */
 
 package com.almostrealism.raytracer.primitives;
