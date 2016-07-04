14c52671ed47519e60bccca243545c0d71db820f
ashesfall <ashesfall@almostrealism.com>
2016-07-03 17:40:32 -0700

    Introduced WebTreeNode, which combines some functionality required for Swing JTree with some functionality required for rendering JSON tree data in a web page.

---
+++ Common/src/com/almostrealism/ui/WebTreeNode.java
@@ -0,0 +1,14 @@
+package com.almostrealism.ui;
+
+import javax.swing.tree.TreeNode;
+
+public interface WebTreeNode<T> extends TreeNode, Iterable<T> {
+	/**
+	 * Return the name of a field which will contain children
+	 * when this object is mapped to JSON or XML. This prevents
+	 * cluttering the exported data with duplicate data for
+	 * children when children are already being mapped to a
+	 * JSON/XML field.
+	 */
+	public String getChildrenFieldName();
+}
