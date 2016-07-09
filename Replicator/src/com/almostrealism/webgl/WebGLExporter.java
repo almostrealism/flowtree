ecb689a86cb86476d448fc7a06eb8f7bebabcdce
ashesfall <ashesfall@almostrealism.com>
2016-07-09 16:18:07 -0700

    Testing stuff.

--- Replicator/src/com/almostrealism/webgl/WebGLExporter.java
+++ Replicator/src/com/almostrealism/webgl/WebGLExporter.java
@@ -15,11 +15,19 @@ public class WebGLExporter {
 		
 		// TODO  Camera config, etc
 		
-		HTMLFragment geometry = new HTMLFragment();
+		HTMLFragment geometry = new HTMLFragment(HTMLFragment.Type.SCRIPT);
+		int index = 0;
 		
 		for (WebGLRenderable r : renderables) {
 			geometry.add(r.getWebGLGeometry().getWebGLContent());
 			geometry.add(r.getWebGLMaterial().getWebGLContent());
+			
+			final String mesh = "renderable" + index;
+			
+			geometry.add(() -> "var " + mesh + " = new THREE.Mesh( geometry, material );");
+			geometry.add(() -> "scene.add(" + mesh + ")");
+			
+			index++;
 		}
 		
 		p.add(geometry);
