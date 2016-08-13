c5b6601f5fad48331d3209285d605610ce22fb66
ashesfall <ashesfall@almostrealism.com>
2016-08-12 20:48:45 -0700

    More work on GraphPersist.

--- GraphPersist/src/io/almostrealism/query/SimpleQuery.java
+++ GraphPersist/src/io/almostrealism/query/SimpleQuery.java
@@ -1,12 +1,21 @@
 package io.almostrealism.query;
 
+import java.util.Collection;
+import java.util.Hashtable;
+
 /**
  * {@link SimpleQuery} maps named columns to the POJO fields that should be populated.
  *
  * @author  Michael Murray
  */
-public class SimpleQuery<V> {
-    public SimpleQuery() {
+public abstract class SimpleQuery<D, K, V> implements Query<D, K, V> {
+	private Hashtable<String, String> map = new Hashtable<>();
 
-    }
+	protected String query;
+
+	public SimpleQuery(String q) { query = q; }
+
+	public void put(String column, String fieldName) { map.put(column, fieldName); }
+
+	public String get(String name) { return map.get(name); }
 }
---
+++ GraphPersist/src/io/almostrealism/sql/SQLSelect.java
@@ -0,0 +1,12 @@
+package io.almostrealism.sql;
+
+import io.almostrealism.query.SimpleQuery;
+
+import java.util.Properties;
+
+/**
+ * @author  Michael Murray
+ */
+public class SQLSelect<V> extends SimpleQuery<ComboPooledDataSource, String, V> {
+	public static SQLSelect prepare(String query, Properties )
+}
