d7fba980309a7331ae555fbf4e925590a217b937
ashesfall <ashesfall@almostrealism.com>
2016-08-13 09:33:00 -0700

    Expanding on SQLSelect, the most basic/common query type.

--- GraphPersist/src/io/almostrealism/sql/SQLSelect.java
+++ GraphPersist/src/io/almostrealism/sql/SQLSelect.java
@@ -8,5 +8,11 @@ import java.util.Properties;
  * @author  Michael Murray
  */
 public class SQLSelect<V> extends SimpleQuery<ComboPooledDataSource, String, V> {
-	public static SQLSelect prepare(String query, Properties )
+	private SQLSelect(String query, Properties columns) {
+
+	}
+
+	public static SQLSelect prepare(String query, Properties columns) {
+		return new SQLSelect(query, )
+	}
 }
