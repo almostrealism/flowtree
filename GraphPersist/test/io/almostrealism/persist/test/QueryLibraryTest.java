eb762730037593075092d73af4377faea4dcfc82
ashesfall <ashesfall@almostrealism.com>
2016-08-16 17:27:46 -0700

    Created failing QueryLibraryTest to begin unit testing for query library.

---
+++ GraphPersist/test/io/almostrealism/persist/test/QueryLibraryTest.java
@@ -0,0 +1,12 @@
+package io.almostrealism.persist.test;
+
+import static org.junit.Assert.*;
+
+/**
+ */
+public class QueryLibraryTest {
+	@org.junit.Test
+	public void main() throws Exception {
+		assertTrue("Failed", false);
+	}
+}
