05bccc69c05b758336223c762a5da3f06c69efdb
ashesfall <ashesfall@almostrealism.com>
2016-08-24 22:20:17 -0700

    Eliminated the need for com.almostrealism.feedgrow.tools package.

--- FeedGrowth/src/com/almostrealism/feedgrow/HealthTestTool.java
+++ FeedGrowth/src/com/almostrealism/feedgrow/HealthTestTool.java
@@ -14,7 +14,7 @@
  * limitations under the License.
  */
 
-package com.almostrealism.feedgrow.tools;
+package com.almostrealism.feedgrow;
 
 import java.io.FileInputStream;
 import java.io.FileNotFoundException;
