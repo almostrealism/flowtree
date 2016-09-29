67ea38dc602154f85f81be999b0c09c169b70b17
ashesfall <ashesfall@almostrealism.com>
2016-09-28 18:17:40 -0700

    WebGL export for faces to three.js.

--- Rings/src/com/almostrealism/webgl/WebGLMeshGeometry.java
+++ Rings/src/com/almostrealism/webgl/WebGLMeshGeometry.java
@@ -29,14 +29,26 @@ public class WebGLMeshGeometry implements WebGLExportable {
 	public HTMLContent getWebGLContent() {
 		return () -> {
 			StringBuffer buf = new StringBuffer();
+			buf.append("var geometry = new THREE.Geometry();\n");
 			buf.append("geometry.vertices.push(");
 			
 			for (int i = 0; i < data.getVertexCount(); i++) {
 				buf.append(asThreeVector(data, i));
 				if (i < (data.getVertexCount() - 1)) buf.append(",");
+				buf.append("\n");
 			}
 			
-			buf.append(");");
+			buf.append(");\n");
+			
+			buf.append("geometry.faces.push(");
+			
+			for (int i = 0; i < data.getTriangleCount(); i++) {
+				buf.append(asThreeFace(data.getTriangle(i)));
+				if (i < (data.getTriangleCount() - 1)) buf.append(",");
+				buf.append("\n");
+			}
+			
+			buf.append(");\n");
 			
 			return buf.toString();
 		};
@@ -44,5 +56,9 @@ public class WebGLMeshGeometry implements WebGLExportable {
 	
 	private static String asThreeVector(Mesh.VertexData v, int index) {
 		return "new THREE.Vector3(" + v.getX(index) + ", " + v.getY(index) + ", " + v.getZ(index) + ")";
+	}
+	
+	private static String asThreeFace(int triangle[]) {
+		return "new THREE.Face3(" + triangle[0] + ", " + triangle[1] + ", " + triangle[2] + ")";
 	}
 }
