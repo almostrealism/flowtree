8dcdd3238da38022008cf18cd6d79fa5f4ef3a94
ashesfall <ashesfall@almostrealism.com>
2017-01-20 20:59:21 -0800

    filter method may throw CloneNotSupportedException.

--- GraphPersist/src/io/almostrealism/persist/ValueFilter.java
+++ GraphPersist/src/io/almostrealism/persist/ValueFilter.java
@@ -3,5 +3,5 @@ package io.almostrealism.persist;
 import java.util.Collection;
 
 public abstract class ValueFilter<V> {
-	public abstract Collection<V> filter(Collection<V> values);
+	public abstract Collection<V> filter(Collection<V> values) throws CloneNotSupportedException;
 }
