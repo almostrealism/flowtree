4c365e7677b76308c5e7baebb1a8335403f9388a
ashesfall <ashesfall@almostrealism.com>
2017-06-30 14:50:43 -0700

    Migrated NetworkTreeNode into the UI package.

--- FlowTree/src/org/almostrealism/flow/ui/NetworkTreeNode.java
+++ FlowTree/src/org/almostrealism/flow/ui/NetworkTreeNode.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2016 Michael Murray
+ * Copyright 2017 Michael Murray
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -14,16 +14,7 @@
  * limitations under the License.
  */
 
-/*
- * Copyright (C) 2005-06  Mike Murray
- *
- *  All rights reserved.
- *  This document may not be reused without
- *  express written permission from Mike Murray.
- *
- */
-
-package org.almostrealism.flow.tree;
+package org.almostrealism.flow.ui;
 
 import io.almostrealism.tree.ui.TreeNode;
 
