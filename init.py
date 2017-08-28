7f0e1ff87bcb0d295d0d79148011ae0ac771e4fd
ashesfall <ashesfall@almostrealism.com>
2017-08-28 07:57:30 -0700

    Purged the init script of msched-specific code.

--- init.py
+++ init.py
@@ -18,42 +18,6 @@ with open('heart_beat', 'w') as outfile:
 # Start cron
 os.system('/etc/init.d/crond start')
 
-# -----------ssh
-# Download the git credentials and add them to ssh-agent
-
-if not os.path.exists(home + '/.ssh'):
-    # This is sort of redundent as we assume ssh is installed in which case this dir exists
-    os.system('mkdir ' + home + '/.ssh')
-
-os.chdir(home + '/.ssh')
-
-for p in ['prv', 'pub']:
-    response = urllib2.urlopen('http://msched.us-west-2.elasticbeanstalk.com/'+p+'_git_key')
-    infile = response.read()
-
-    with open(p+'_git_key', 'w') as outfile:
-        outfile.write(infile)
-
-os.system('mv '+home+'/.ssh/pub_git_key '+home+'/.ssh/git_gen.pub')
-os.system('mv '+home+'/.ssh/prv_git_key '+home+'/.ssh/git_gen')
-
-# Add ssh keys
-
-bash_cmd="""
-eval `ssh-agent -s`;
-chmod 400 """+home+"""/.ssh/git_gen;
-ssh-add """+home+"""/.ssh/git_gen;
-cat > """+home+"""/.ssh/config<<-EOM
-Host bitbucket.org
- IdentityFile """+home+"""/.ssh/git_gen
-"""
-os.system(bash_cmd)
-os.system('chmod 600 '+home+'/.ssh/config;')
-# -----------git
-# Sync repos
-os.system('cd '+home)
-os.system('git pull git@bitbucket.org:terraai/msched.git/')
-
 # --------------- airflow.cfg
 # Pull airflow.cfg
 # afc = urllib.URLopener()
