8babcd978833eea92e6be5dc01b62024714165a9
ashesfall <ashesfall@almostrealism.com>
2017-09-30 11:02:51 -0700

    Working towards a functioning set of airflow plugins.

--- init.py
+++ init.py
@@ -20,24 +20,25 @@ os.system('/etc/init.d/crond start')
 # --------------- airflow.cfg
 # Pull airflow.cfg
 # afc = urllib.URLopener()
-# afc.retrieve('http://msched.us-west-2.elasticbeanstalk.com/AirflowConfig', '~/airflow/airflow.cfg')
+# afc.retrieve('http://ms   ched.us-west-2.elasticbeanstalk.com/AirflowConfig', '~/airflow/airflow.cfg')
 # os.system()
 # --------------- TEMP
 # move airflow.cfg to /airflow
 # os.system('rm /airflow/airflow.cfg')
 
 os.system('sudo systemctl start postgresql')
-os.system('airflow initdb')
-
 os.system('java -cp /root/FlowTree-0.1-rc.jar:/root/TreeView-0.1-rc.jar:/root/Common-0.1-rc.jar:/root/hsqldb-2.3.4.jar:/root/jsch-0.1.53.jar org.almostrealism.flow.Server /root/flowtree.conf -p &')
 
 os.system('/root/apache-activemq-5.15.0/bin/activemq start')
 
+os.system('sudo mkdir /airflow/')
 os.system('sudo mkdir /airflow/plugins/')
-os.system('cp /root/executor/flowtree_executor.py /airflow/plugins/flowtree_executor.py')
+os.system('cp /root/executor/flowtree_plugin.py /airflow/plugins/flowtree_plugin.py')
+os.system('cp /root/executor/test_plugin.py /airflow/plugins/test_plugin.py')
 os.system('rm /airflow/airflow.cfg')
 os.system('sudo cp /root/executor/airflow.cfg /airflow');
 
 os.system('airflow initdb')
 
+os.system('ls /airflow/plugins/')
 os.system('airflow webserver')
