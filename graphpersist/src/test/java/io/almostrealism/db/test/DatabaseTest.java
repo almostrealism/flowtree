8cbf05d650172e3d35e27479e4738cf900ae7388
Michael Murray <michael@almostrealism.com>
2025-11-28 09:59:48 -0800

    Removed type argument from PackedCollection

--- graphpersist/src/test/java/io/almostrealism/db/test/DatabaseTest.java
+++ graphpersist/src/test/java/io/almostrealism/db/test/DatabaseTest.java
@@ -8,15 +8,15 @@ import org.almostrealism.util.TestFeatures;
 import org.junit.Test;
 
 public class DatabaseTest implements TestFeatures {
-	private static PackedCollection<?> scalarValue(double value) {
-		PackedCollection<?> s = new PackedCollection<>(1);
+	private static PackedCollection scalarValue(double value) {
+		PackedCollection s = new PackedCollection(1);
 		s.setMem(0, value);
 		return s;
 	}
 
 	@Test
 	public void storeAndRetrieve() {
-		Tensor<PackedCollection<?>> t = new Tensor<>();
+		Tensor<PackedCollection> t = new Tensor<>();
 		t.insert(scalarValue(1), 0, 0);
 		t.insert(scalarValue(2), 0, 1);
 		t.insert(scalarValue(3), 0, 2);
@@ -26,7 +26,7 @@ public class DatabaseTest implements TestFeatures {
 
 		GraphPersist.local().save("/test", t.pack());
 
-		PackedCollection<?> r = GraphPersist.local().read("/test", new TraversalPolicy(2, 3, 1));
+		PackedCollection r = GraphPersist.local().read("/test", new TraversalPolicy(2, 3, 1));
 		assertEquals("Value at index 2", 3.0, r.toDouble(2));
 		assertEquals("Value at index 4", 5.0, r.toDouble(4));
 	}
